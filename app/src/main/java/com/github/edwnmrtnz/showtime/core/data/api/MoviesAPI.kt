5375503b3bed7bdefe8c018a9d8d5d0ab2580729
Edwin Martinez <dev.edwinmartinez@gmail.com>
2023-06-11 10:23:53 +0800

    Remove BASE_URL

--- app/src/main/java/com/github/edwnmrtnz/showtime/core/data/api/MoviesAPI.kt
+++ app/src/main/java/com/github/edwnmrtnz/showtime/core/data/api/MoviesAPI.kt
@@ -18,6 +18,6 @@ interface MoviesAPI {
     @GET("3/movie/upcoming")
     suspend fun getUpcoming(@QueryMap params: HashMap<String, String>): MoviesResponse
 
-    @GET("https://api.themoviedb.org/3/movie/{movie_id}?language=en-US&append_to_response=casts")
+    @GET("3/movie/{movie_id}?language=en-US&append_to_response=casts")
     suspend fun get(@Path("movie_id") id: Int): MovieResponse
 }
